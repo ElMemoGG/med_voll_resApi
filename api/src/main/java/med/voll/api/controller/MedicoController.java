e4ca4bbb489263b815db991c036c76f959af5359
ElMemoGG <omem.emanuel@gmail.com>
2023-09-07 16:54:36 -0600

    fist commit

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,50 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController { // DTO data transfer object :: nombre del patro que estamos usando controller -> repository-> record -> model
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+    @GetMapping
+    public Page<DatosListadoMedico> listadoMedios(@PageableDefault(size = 10) Pageable paginacion){
+        //return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+        return medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico){
+        Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+        medico.actuliazarDatos(datosActualizarMedico);
+    }
+
+    // delete logico
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void eliminarMedico(@PathVariable Long id){
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.desactivarMedico();
+
+    }
+    //delete de base de datos
+   /* public void eliminarMedico(@PathVariable Long id){
+        Medico medico = medicoRepository.getReferenceById(id);  Eliminar registro de la base de datos
+        medicoRepository.delete(medico);
+
+    }*/
+}
